96ab63b3ccccaf066a91be201bdcbe30a33f58f3
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-12-19 00:41:27 +0900

    add: day5 wip.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::{
     fmt,
     net::{Ipv4Addr, Ipv6Addr},
@@ -6,12 +7,14 @@ use std::{
 
 use axum::{
     extract::Query,
-    http::{header, StatusCode},
-    response::IntoResponse,
-    routing::get,
+    http::{header, HeaderMap, StatusCode},
+    response::{IntoResponse, Response},
+    routing::{get, post},
     Router,
 };
+use cargo_manifest::Manifest;
 use serde::{de, Deserialize, Deserializer};
+use toml::Value;
 
 async fn hello_world() -> &'static str {
     "Hello, bird!"
@@ -127,6 +130,75 @@ async fn extract_ipv6_key(ipv6: Query<Ipv6Params>) -> String {
     .to_string()
 }
 
+async fn parse_manifest(headers: HeaderMap, body: String) -> Response {
+    // dbg!(headers);
+    let content_type = headers
+        .get(header::CONTENT_TYPE)
+        .expect("Error: content-type isn't exist in header.")
+        .to_str()
+        .expect("Error: content-type isn't parsable to string.");
+
+    let mut ret = vec![];
+    let manifest: Manifest = match content_type {
+        "application/toml" => toml::from_str(&body).unwrap(),
+        "application/yaml" => serde_yaml::from_str(&body).unwrap(),
+        "application/json" => serde_json::from_str(&body).unwrap(),
+        _ => {
+            return (StatusCode::UNSUPPORTED_MEDIA_TYPE).into_response();
+        }
+    };
+
+    let Some(package) = manifest.package else {
+        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+    };
+    let Some(keywords) = package.keywords else {
+        return (StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response();
+    };
+    if keywords
+        .as_local()
+        .unwrap()
+        .iter()
+        .any(|x| !x.to_string().contains("Christmas 2024"))
+    {
+        return (StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response();
+    }
+    let Some(metadata) = package.metadata else {
+        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+    };
+    let Some(orders) = metadata.get("orders") else {
+        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+    };
+
+    if let Value::Array(orders) = orders {
+        for order in orders {
+            match order {
+                Value::Table(order) => {
+                    let raw_item = order.get("item").unwrap().to_string();
+                    let trim_item = raw_item.trim_matches('"');
+                    let raw_quantity = order.get("quantity").unwrap().to_string();
+                    let trim_quantity = raw_quantity.trim_matches('"');
+                    if let (Ok(item), Ok(quantity)) =
+                        (trim_item.parse::<String>(), trim_quantity.parse::<u32>())
+                    {
+                        ret.push(format!("{}: {}", item, quantity));
+                    }
+                }
+                _ => panic!("Error: Input format is not correct."),
+            }
+        }
+    } else {
+        panic!("Error: Input format is not correct.");
+    }
+    // dbg!(&ret);
+
+    if ret.len() == 0 {
+        StatusCode::NO_CONTENT.into_response()
+    } else {
+        ret.push(String::new());
+        (StatusCode::OK, ret.join("\n")).into_response()
+    }
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
@@ -135,7 +207,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/2/dest", get(ipv4_encryption))
         .route("/2/key", get(extract_ipv4_key))
         .route("/2/v6/dest", get(ipv6_encryption))
-        .route("/2/v6/key", get(extract_ipv6_key));
+        .route("/2/v6/key", get(extract_ipv6_key))
+        .route("/5/manifest", post(parse_manifest));
 
     Ok(router.into())
 }
