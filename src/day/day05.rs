55a4beb210666e479a5b8053514657088c0c74a8
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-12-30 08:02:02 +0900

    fix: day5

--- src/day/day05.rs
+++ src/day/day05.rs
@@ -1,11 +1,14 @@
 use axum::{
     http::{header, HeaderMap, StatusCode},
-    response::{IntoResponse, Response},
+    response::IntoResponse,
 };
 use cargo_manifest::Manifest;
 use toml::Value;
 
-pub async fn parse_manifest(headers: HeaderMap, body: String) -> Response {
+pub async fn parse_manifest(
+    headers: HeaderMap,
+    body: String,
+) -> Result<impl IntoResponse, impl IntoResponse> {
     // dbg!(headers);
     let content_type = headers
         .get(header::CONTENT_TYPE)
@@ -15,61 +18,70 @@ pub async fn parse_manifest(headers: HeaderMap, body: String) -> Response {
 
     let mut ret = vec![];
     let manifest: Manifest = match content_type {
-        "application/toml" => toml::from_str(&body).unwrap(),
-        "application/yaml" => serde_yaml::from_str(&body).unwrap(),
-        "application/json" => serde_json::from_str(&body).unwrap(),
+        "application/toml" => toml::from_str(&body)
+            .map_err(|_| (StatusCode::BAD_REQUEST, "Invalid manifest").into_response())?,
+        "application/yaml" => serde_yaml::from_str(&body)
+            .map_err(|_| (StatusCode::BAD_REQUEST, "Invalid manifest").into_response())?,
+        "application/json" => serde_json::from_str(&body)
+            .map_err(|_| (StatusCode::BAD_REQUEST, "Invalid manifest").into_response())?,
         _ => {
-            return (StatusCode::UNSUPPORTED_MEDIA_TYPE).into_response();
+            return Err((StatusCode::UNSUPPORTED_MEDIA_TYPE).into_response());
         }
     };
 
     let Some(package) = manifest.package else {
-        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+        return Err((StatusCode::BAD_REQUEST, "Invalid manifest").into_response());
     };
     let Some(keywords) = package.keywords else {
-        return (StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response();
+        return Err((StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response());
     };
-    if keywords
+    if !keywords
         .as_local()
         .unwrap()
         .iter()
-        .any(|x| !x.to_string().contains("Christmas 2024"))
+        .any(|x| x == "Christmas 2024")
     {
-        return (StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response();
+        return Err((StatusCode::BAD_REQUEST, "Magic keyword not provided").into_response());
     }
     let Some(metadata) = package.metadata else {
-        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+        return Ok(StatusCode::NO_CONTENT.into_response());
     };
     let Some(orders) = metadata.get("orders") else {
-        return (StatusCode::BAD_REQUEST, "Invalid manifest").into_response();
+        return Ok(StatusCode::NO_CONTENT.into_response());
     };
 
     if let Value::Array(orders) = orders {
         for order in orders {
             match order {
                 Value::Table(order) => {
-                    let raw_item = order.get("item").unwrap().to_string();
-                    let trim_item = raw_item.trim_matches('"');
-                    let raw_quantity = order.get("quantity").unwrap().to_string();
+                    let Some(raw_item) = order.get("item") else {
+                        continue;
+                    };
+                    let raw_item = raw_item.to_string();
+                    let trim_item = raw_item.trim_matches('"').trim_start_matches('\n');
+                    let Some(raw_quantity) = order.get("quantity") else {
+                        continue;
+                    };
+                    let raw_quantity = raw_quantity.to_string();
                     let trim_quantity = raw_quantity.trim_matches('"');
-                    if let (Ok(item), Ok(quantity)) =
-                        (trim_item.parse::<String>(), trim_quantity.parse::<u32>())
-                    {
-                        ret.push(format!("{}: {}", item, quantity));
-                    }
+                    let Ok(item) = trim_item.parse::<String>();
+                    let Ok(quantity) = trim_quantity.parse::<u32>() else {
+                        continue;
+                    };
+                    // dbg!(&order);
+                    ret.push(format!("{}: {}", item, quantity));
                 }
                 _ => panic!("Error: Input format is not correct."),
             }
         }
     } else {
-        panic!("Error: Input format is not correct.");
+        return Ok(StatusCode::NO_CONTENT.into_response());
     }
     // dbg!(&ret);
 
     if ret.len() == 0 {
-        StatusCode::NO_CONTENT.into_response()
+        Ok(StatusCode::NO_CONTENT.into_response())
     } else {
-        ret.push(String::new());
-        (StatusCode::OK, ret.join("\n")).into_response()
+        Ok((StatusCode::OK, ret.join("\n")).into_response())
     }
 }
